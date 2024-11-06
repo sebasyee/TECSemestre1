267bd8518d3bbbe71719021d721d7c83d3736db9
sebasyee <164583352+sebasyee@users.noreply.github.com>
2024-11-05 23:42:02 -0700

    Add files via upload

---
+++ Ejercicio19/src/NumTelefono.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NumTelefono {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String numTel,numFormat;
+
+        for (int i = 0; i < 8; i++) {
+            do {
+                System.out.println("Ingresa un número de teléfono de 10 dígitos");
+                numTel = sc.nextLine();
+                
+                if (numTel.length() != 10) {
+                    System.out.println("El número ingresado no tiene 10 dígitos. Inténtalo de nuevo.");
+                }
+            } 
+            while (numTel.length() != 10);
+            numFormat = "("+ numTel.substring(0, 3)+")"+" "+numTel.substring(3, 6)+"-"+numTel.substring(6, 8)+"-"+numTel.substring(8, 10);
+            System.out.println("Numero con formato: "+numFormat);
+
+        }
+    }
+}
