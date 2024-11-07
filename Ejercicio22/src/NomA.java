eae942f4f7c9fd4d293b620fbedf49f9376f99bf
sebasyee <164583352+sebasyee@users.noreply.github.com>
2024-11-06 20:45:13 -0700

    Add files via upload

---
+++ Ejercicio22/src/NomA.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class NomA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		String nom,letra;
+		int a=0;
+		
+		for(int i=0;i<3;i++) {
+			System.out.println("Ingresa un nombre");
+			nom = sc.nextLine();
+		    
+			if(nom.charAt(0)=='a'||nom.charAt(0)=='A') {
+				a++;
+			}
+		}
+			System.out.println("Nombres que empiezan con A: "+a);
+		
+	}
+
+}
