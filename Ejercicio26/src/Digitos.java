01df81c5ec157161948f59cfff9d0a0817da081c
sebasyee <164583352+sebasyee@users.noreply.github.com>
2024-11-06 23:38:33 -0700

    Add files via upload

---
+++ Ejercicio26/src/Digitos.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Digitos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa un texto");
+        String text = sc.nextLine();
+        int numDigitos = 0;
+        int digitos = text.length();
+        char caracteres;
+
+        for (int i = 0; i < digitos; i++) {
+            caracteres = text.charAt(i);
+
+            if (Character.isAlphabetic(caracteres)) {
+                numDigitos++;
+            }
+        }
+
+        System.out.println("El número de dígitos es: " + numDigitos);
+    }
+}
